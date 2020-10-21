7cc1ebfc38049c935051e9a96aaaef0801e26cc7
rquirogagUcentral <rquirogag@ucentral.edu.co>
2020-10-20 20:27:28 -0500

    Se ajusta metodo crearConexion de la clase singleton para que se capture en ella la información contenida en el archivo DBMySQL.properties

--- src/main/java/com/bancosoft/ws/rest/DAO/Singleton.java
+++ src/main/java/com/bancosoft/ws/rest/DAO/Singleton.java
@@ -36,42 +36,30 @@ public class Singleton {
     }
 	    
     public Connection cadenaConexion() {
-        try {
-        String connectionString = createConnectionString();
-        //String url = "jdbc:mysql://ingsoftwaremsql.mysql.database.azure.com:3306/banco?useSSL=true&requireSSL=false";
-        //Connection connection = DriverManager.getConnection(url,"administrador@ingsoftwaremsql","Grupo&7Software");
-        String[] instancia = connectionString.split("\\?");
-        String url = instancia[0];
-        String[] login = instancia[1].split("\\&");
-        String[] usuario = login[0].split("\\=");
-        String user = usuario[1];
-        String[] contrasena = login[1].split("\\=");
-        String pass = contrasena[1]+"&"+login[2];
-        Connection connection = DriverManager.getConnection(url,user,pass);
-        System.out.println("Connection class ===>"+connection.getClass().getName());
-        return connection;
-        }catch (Exception e){
+        try 
+        {
+        	Properties prop = PropertiesUtil.loadProperty(DB_PROPERTIES);
+	        String host = prop.getProperty(DB_HOST_PROP);
+	        String port = prop.getProperty(DB_PORT_PROP);
+	        String db = prop.getProperty(DB_NAME_PROP);
+	        String user = prop.getProperty(DB_USER_PROP);
+	        String password = prop.getProperty(DB_PASSWORD_PROP);
+	        
+	        String connectionString = "jdbc:mysql://"+host+":"+port+"/"+db;
+	        System.out.println("ConnectionString ===>"+ connectionString);
+	        
+	        
+	        Connection connection = DriverManager.getConnection(connectionString,user,password);
+	        System.out.println("Connection class ===>"+connection.getClass().getName());
+	        return connection;
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
             return null;
         }         
     }    
 	    
-private String createConnectionString(){
-        
-        Properties prop = PropertiesUtil.loadProperty(DB_PROPERTIES);
-        String host = prop.getProperty(DB_HOST_PROP);
-        String port = prop.getProperty(DB_PORT_PROP);
-        String db = prop.getProperty(DB_NAME_PROP);
-        String user = prop.getProperty(DB_USER_PROP);
-        String user2 = prop.getProperty(DB_USER2_PROP);
-        String password = prop.getProperty(DB_PASSWORD_PROP);
-        
-        String connectionString = "jdbc:mysql://"+host+":"+port+"/"+db+"?user="+user+"@"+user2+"&password="+password;
-        System.out.println("ConnectionString ===>"+ connectionString);
-        return connectionString;       
-     }
-
-	
 	public static Connection getConnection() {
         if(conn==null)
         {
